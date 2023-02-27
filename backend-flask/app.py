7704aae34a48830ad0946e0b03a2292d8c49abec
djoelmoya <joelmoya31@gmail.com>
2023-02-27 02:39:00 +0000

    fix import new class notifications_activities.

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -12,6 +12,7 @@ from services.message_groups import *
 from services.messages import *
 from services.create_message import *
 from services.show_activity import *
+from services.notifications_activities import *
 
 app = Flask(__name__)
 frontend = os.getenv('FRONTEND_URL')
